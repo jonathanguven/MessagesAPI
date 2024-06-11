2a38153a702ff8d7d43021526de5afba95d1246f
Jonathan <jonathanguven@gmail.com>
2024-06-11 01:02:05 -0700

    set up script

---
+++ python/connect.py
@@ -0,0 +1,16 @@
+import sseclient
+import requests
+
+def sseConnect(url):
+    response = requests.get(url, stream=True)
+
+    response.raise_for_status()
+
+    client = sseclient.SSEClient(response)
+
+    for event in client.events():
+        print(f"Message received: {event.data}")
+
+if __name__ == "__main__":
+    sse_url = 'http://localhost:3000/listen?room=general'
+    sseConnect(sse_url)
